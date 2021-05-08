8f848c4608608c3ee9cb0695042ffdbe085ba5ef
kalpana1 <dkdevi.dkd@gmail.com>
2021-05-08 13:33:41 +0530

    counter with dynamic pages

--- src/App.js
+++ src/App.js
@@ -3,18 +3,32 @@ import './App.css';
 
 function App() {
   const [count, setCount] = useState(0);
+  const [color, setColor] = useState();
+
+  function Upcount() {
+    setCount(count + 1);
+    setColor(true);
+
+  }
 
 
+  function Downcount() {
+    setColor(false);
+    setCount(count - 1);
+
+  }
 
 
   return (
     <div className="App">
       <h3>The count is:{count}</h3>
       <br />
-      <button className='btn' onClick={() => setCount(count + 1)}>Upcounter</button><br />
-      <button className='btn' onClick={() => setCount(count - 1)}>Downcounter</button>
+      <button className={color ? 'btn-upcount' : 'btn'} onClick={Upcount}>Upcounter</button><br />
+      <button className={color ? 'btn' : 'btndowncount'} onClick={Downcount}>Downcounter</button>
     </div>
   );
 }
 
 export default App;
+//<button className='btn' onClick={() => setCount(count + 1)}>Upcounter</button><br />
+//<button className='btn' onClick={() => setCount(count - 1)}>Downcounter</button>
