3331b59c1e79d1cf40f00c922e94bbc4f959fcf2
kalpana1 <dkdevi.dkd@gmail.com>
2021-05-08 14:05:32 +0530

    created components folder

--- src/App.js
+++ src/App.js
@@ -1,35 +1,15 @@
-import { useState } from 'react';
 import './App.css';
+import Counter from './components/Counter';
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [color, setColor] = useState();
-
-  function Upcount() {
-    setCount(count + 1);
-    setColor(true);
-
-  }
-
-
-  function Downcount() {
-    setCount(count - 1);
-    setColor(false);
-
-
-  }
 
 
   return (
     <div className="App">
-      <h3>The count is:{count}</h3>
-      <br />
-      <button className={color ? 'btn-upcount' : 'btn'} onClick={Upcount}>Upcounter</button><br />
-      <button className={color ? 'btn' : 'btndowncount'} onClick={Downcount}>Downcounter</button>
+      <Counter />
+
     </div>
   );
 }
 
 export default App;
-//<button className='btn' onClick={() => setCount(count + 1)}>Upcounter</button><br />
-//<button className='btn' onClick={() => setCount(count - 1)}>Downcounter</button>
