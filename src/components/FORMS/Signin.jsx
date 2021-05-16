e670ebaaf5e8d4bcf44763199244fb4466fda85b
kalpana1 <dkdevi.dkd@gmail.com>
2021-05-16 13:13:40 +0530

    controlled responsive from

--- src/components/FORMS/Signin.jsx
+++ src/components/FORMS/Signin.jsx
@@ -10,6 +10,8 @@ function Signin() {
         phonenumber: ''
     });
 
+    const [display, setDisplay] = useState([]);
+
 
     function handleChange(e) {
         const field = e.target.name;
@@ -20,13 +22,10 @@ function Signin() {
 
 
     function handleSubmit(e) {
-
         e.preventDefault();
-
-        const registered = { ...state, id: new Date().getTime().toString() };
-
-
+        const registered = [{ ...state, id: new Date().getTime().toString() }];
         console.log(registered);
+        setDisplay(registered);
         setState({ firstname: "", lastname: "", emailid: "", password: "", phonenumber: "" })
 
     }
@@ -41,26 +40,31 @@ function Signin() {
 
     }
     return (
-        <div>
+        <>
             <form onSubmit={handleSubmit}>
-                <label>First Name:</label>
-                <input type='text' placeholder='enter first name' value={state.firstname} name='firstname' onChange={handleChange} style={formfields} /><br />
-                <label>Last Name:</label>
-                <input type='text' placeholder='enter last name' value={state.lastname} name='lastname' onChange={handleChange} style={formfields} /><br />
-                <label>Email id:</label>
-                <input type='email' placeholder='enter email' value={state.emailid} name='emailid' onChange={handleChange} style={formfields} /><br />
-                <label>password:</label>
-                <input type='password' placeholder='enter password' value={state.password} name='password' onChange={handleChange} style={formfields} /><br />
-                <label>phone number:</label>
-                <input type='number' placeholder='enter phone number' value={state.phonenumber} name='phonenumber' onChange={handleChange} style={formfields} /><br />
+                <label>First Name:</label><br />
+                <input type='text' placeholder='enter first name' value={state.firstname} name='firstname' onChange={handleChange} style={formfields} /><br /><br />
+                <label>Last Name:</label><br />
+                <input type='text' placeholder='enter last name' value={state.lastname} name='lastname' onChange={handleChange} style={formfields} /><br /><br />
+                <label>Email id:</label><br />
+                <input type='email' placeholder='enter email' value={state.emailid} name='emailid' onChange={handleChange} style={formfields} /><br /><br />
+                <label>password:</label><br />
+                <input type='password' placeholder='enter password' value={state.password} name='password' onChange={handleChange} style={formfields} /><br /><br />
+                <label>phone number:</label><br />
+                <input type='number' placeholder='enter phone number' value={state.phonenumber} name='phonenumber' onChange={handleChange} style={formfields} /><br /><br /><br />
                 <input type='submit' />
             </form>
 
             <div>
+                {display.map((data) => {
+                    return (<h3>{data.firstname}<br />{data.lastname}<br />{data.emailid}</h3>)
+
+                })}
 
 
             </div>
-        </div>
+
+        </>
     )
 }
 
