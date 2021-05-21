fc8f77490f09320dd21822212bf541ead5332f40
kalpana1 <dkdevi.dkd@gmail.com>
2021-05-21 20:13:53 +0530

    directly destructring props in fn parameters

--- src/Basics/Welcome.jsx
+++ src/Basics/Welcome.jsx
@@ -13,9 +13,21 @@ import './Welcome.css'
 export default Welcome*/
 
 
-function Welcome(props) {
+/*function Welcome(props) {
     console.log(props);
     const { firstname, lastname, occupation, age } = props; //destructuring
+    return (
+        <div className='welcomediv'>
+            <p>welcome to {firstname} {lastname} aged {age} who is {occupation}</p>
+        </div>
+    )
+}
+
+export default Welcome*/
+
+
+
+function Welcome({ firstname, lastname, occupation, age }) { //as this fn definately expects props as parameters hence directly destructuring
     return (
         <div className='welcomediv'>
             <p>welcome to {firstname} {lastname} aged {age} who is {occupation}</p>
