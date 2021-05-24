ade953daf864c4907e2e4bb889f2362c58bc9641
kalpana1 <dkdevi.dkd@gmail.com>
2021-05-24 21:26:17 +0530

    redux

--- src/Redux/CounterComponent.jsx
+++ src/Redux/CounterComponent.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function CounterComponent() {
+    const [count, setCount] = useState(0)
+
     return (
         <div>
-            redux counter
+            <h4>the count:{count}</h4>
+            <button>increment</button>
+            <button>decrement</button>
         </div>
     )
 }
