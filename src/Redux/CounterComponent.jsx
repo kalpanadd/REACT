4619ade7f1195bf936feb0766d7cfe9899786b12
kalpana1 <dkdevi.dkd@gmail.com>
2021-05-24 21:41:04 +0530

    concept

--- src/Redux/CounterComponent.jsx
+++ src/Redux/CounterComponent.jsx
@@ -2,11 +2,19 @@ import React from 'react'
 
 function CounterComponent({ count, setcount }) {
 
+    function increment() {
+        setcount(count + 1)
+    }
+
+    function decrement() {
+        setcount(count - 1)
+    }
+
     return (
         <div>
             <h4>the count:{count}</h4>
-            <button>increment</button>
-            <button>decrement</button>
+            <button onClick={increment}>increment</button>
+            <button onClick={decrement}>decrement</button>
         </div>
     )
 }
