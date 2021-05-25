c31b93e6654c65f841f49383676f2223a7353a0c
kalpana1 <dkdevi.dkd@gmail.com>
2021-05-25 20:28:07 +0530

    added header component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import './App.css';
+//import './App.css';
 import Counter from './components/COUNTER/Counter';
 import Form from './components/FORMS/Form';
 import Signin from './components/FORMS/Signin';
--- src/NEWPROJECT/COMPOS/MockApp.jsx
+++ src/NEWPROJECT/COMPOS/MockApp.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+
 import Header from './Header';
 import AddContact from './AddContact';
 import ContactList from './ContactList';
